40efa14855c5dd6908cea7dfa9de30acaa27c8e2
ehdgusdl9177 <k23129177@gmail.com>
2024-12-23 13:16:35 +0900

    update POST movie service

--- src/app.service.ts
+++ src/app.service.ts
@@ -34,4 +34,15 @@ export class AppService {
 
     return movie;
   }
+
+  createMovie(title: string) {
+    const movie: Movie = {
+      id: this.idCounter++,
+      title: title,
+    };
+
+    this.movies.push(movie);
+
+    return movie;
+  }
 }
