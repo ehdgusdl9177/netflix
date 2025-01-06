b656b1974c32a0a00a57b8c6b1bdc0912493252c
ehdgusdl9177 <k23129177@gmail.com>
2025-01-06 11:25:33 +0900

    create custom class validator

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -27,11 +27,41 @@ import {
   MinLength,
   NotContains,
   NotEquals,
+  registerDecorator,
+  Validate,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
 } from 'class-validator';
 
 enum MovieGenre {
   Fantasy = 'fantasy',
   Action = 'action',
+}
+
+@ValidatorConstraint()
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    return value.length > 4 && value.length < 8;
+  }
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    return '비밀번호의 길이는 4~8자 여야합니다. 입력된 비밀번호: ($value)';
+  }
+}
+
+function IsPasswordValid(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: PasswordValidator,
+    });
+  };
 }
 
 export class UpdateMovieDto {
@@ -75,5 +105,11 @@ export class UpdateMovieDto {
   // @MinLength(16)
   // @IsUUID()
   // @IsLatLong()
+  // @Validate(PasswordValidator, {
+  //   message: '다른 에러 메세지',
+  // })
+  @IsPasswordValid({
+    message: '다른 메세지',
+  })
   test: string;
 }
