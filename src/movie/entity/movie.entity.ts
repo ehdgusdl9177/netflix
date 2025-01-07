7903953bde66f4fb5bd705e63ba599ccde51fd4d
ehdgusdl9177 <k23129177@gmail.com>
2025-01-07 12:52:31 +0900

    custom transform

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,8 +1,5 @@
-import { Transform } from 'class-transformer';
-
 export class Movie {
   id: number;
   title: string;
-  @Transform(({ value }) => value.toString().toUpperCase())
   genre: string;
 }
